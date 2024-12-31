dac2a19a84138a43f2b1bc33bc7d9ce9fb0dc8b5
DenysDominskyi <denysdominskyi@gmail.com>
2024-12-31 12:50:36 +0100

    codeWars2

--- src/codeWarsTasks/tasks.js
+++ src/codeWarsTasks/tasks.js
@@ -98,3 +98,13 @@ function solution(number) {
   return result;
 }
 
+// task 8
+// Create Phone Number
+
+function createPhoneNumber(numbers){
+  return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`
+}
+
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // => returns "(123) 456-7890"
+
+// task 9
