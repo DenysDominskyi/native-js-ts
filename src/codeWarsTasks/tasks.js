4bedaa39ecb298415912263045b0f1abb4331419
DenysDominskyi <denysdominskyi@gmail.com>
2025-01-14 20:58:55 +0100

    codeWars7

--- src/codeWarsTasks/tasks.js
+++ src/codeWarsTasks/tasks.js
@@ -185,4 +185,7 @@ function duplicateCount(text) {
 // }
 const findOdd = (xs) => xs.reduce((a, b) => a ^ b); //best practices
 
-console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+// console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+
+// task 14
+const isIsogram = (str) => new Set(str.toLowerCase()).size === str.length
